1a7065f0ab745eddb243f8e652b9f4c2457d59e1
Will Franklin <will.franklin@guardian.co.uk>
2013-11-16 19:11:12 +0000

    Show details from getPerson

--- fantasy-parliament/static/script.js
+++ fantasy-parliament/static/script.js
@@ -17,13 +17,7 @@ $('#remove-mps').click(function () {
 
 $('#available-mps, #selected-mps').change(function () {
     var option = $(this).children(':selected').first();
-    var mp = option.data('mp');
-
-    $('.mp-details').each(function () {
-        $(this).html(mp[$(this).data('detail')]);
-    });
-
-    twfy.query('getPerson', {'callback': 'populate_photo', 'id': mp['person_id']});
+    twfy.query('getPerson', {'callback': 'populate_details', 'id': option.val()});
 });
 
 $('#save-mps').click(function () {
@@ -66,7 +60,6 @@ function populate_mps(mps) {
 
     $.each(sortedMPs, function () {
         var ele = $('<option value="' + this.person_id + '" data-party="' + this.party + '">' + this.name + ' [' + this.party + ']</option>');
-        ele.data('mp', this);
 
         if (userMPs.indexOf(this.person_id) !== -1) {
             selectedMPs.append(ele);
@@ -76,6 +69,12 @@ function populate_mps(mps) {
     });
 }
 
-function populate_photo(person) {
-    $('.mp-photo').attr('src', 'http://www.theyworkforyou.com' + person[0].image);
+function populate_details(person) {
+    person = person[0]
+
+    $('.mp-photo').attr('src', 'http://www.theyworkforyou.com' + person.image);
+
+    $('.mp-details').each(function () {
+        $(this).html(person[$(this).data('detail')]);
+    });
 }
