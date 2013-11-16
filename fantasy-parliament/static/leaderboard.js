db134700b63746a48f908e119b4cb349de08faf6
Julian Fitzell <jfitzell@gmail.com>
2013-11-16 21:46:58 +0000

    Add functional display of scoring details (needs styling)

--- fantasy-parliament/static/leaderboard.js
+++ fantasy-parliament/static/leaderboard.js
@@ -5,6 +5,18 @@ $('.user').click(function () {
 
 	$.getJSON('/user/' + email + '/scoring')
             .then(function (data) {
-                scoring.html(data);
+                updateScoring(data);
             });
 });
+
+function updateScoring(data) {
+	scoring.html('');
+
+	$.each(data, function(mp, scores) {
+		scoring.append('<dt>' + mp + '<dt>');
+
+		$.each(scores, function(scoreName, scoreValue) {
+			scoring.append('<dd><span class="scoreName">' + scoreName + '</span><span class="scoreValue">' + scoreValue + '</span></dd>');
+		});
+	});
+};
