2d05dec46766905a10d12289a39cdfc4052d3d0b
Will Franklin <will.franklin@guardian.co.uk>
2013-11-17 15:05:19 +0000

    Fixed error

--- fantasy-parliament/static/leaderboard.js
+++ fantasy-parliament/static/leaderboard.js
@@ -14,7 +14,7 @@ function updateScoring(data) {
 
 	$.each(data, function(memberId, scores) {
 		var name = mps[memberId] ? mps[memberId].full_name : memberId;
-		scoring.append('<dt>' + name + '<dt>');
+		scoring.append('<dt>' + name + '</dt>');
 
 		$.each(scores, function() {
 			scoring.append('<dd data-type="' + this.type + '" data-score="' + this.score + '"><span class="scoreName">' + this.description + '</span><span class="scoreValue">' + this.score + '</span></dd>');
